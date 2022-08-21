cf91fc981e9fcb23e9c3b2db84c9732730f29583
Curtis Larson <quackware@gmail.com>
2022-08-21 14:02:25 -0400

    fix: fix bad typings and make deno compatible, remove tests for now

--- src/index.ts
+++ src/index.ts
@@ -1,154 +1,155 @@
-import { format } from './sanitization.js'
-export { format } from './sanitization.js'
-export { hex } from './text.js'
-import { decode } from './text.js'
-import { Version } from './version.js'
+import { format } from "./sanitization.ts";
+export { format } from "./sanitization.ts";
+export { hex } from "./text.ts";
+import { decode } from "./text.ts";
+import { Version } from "./version.ts";
 
-type ReqInit = Pick<RequestInit, 'method' | 'headers'> & {
-  body: string
-}
+type ReqInit = Pick<RequestInit, "method" | "headers"> & {
+  body: string;
+};
 
-type Row = Record<string, unknown>
+type Row = Record<string, unknown>;
 
 interface VitessError {
-  message: string
-  code: string
+  message: string;
+  code: string;
 }
 
 export class DatabaseError extends Error {
-  body: VitessError
-  status: number
+  body: VitessError;
+  status: number;
   constructor(message: string, status: number, body: VitessError) {
-    super(message)
-    this.status = status
-    this.name = 'DatabaseError'
-    this.body = body
+    super(message);
+    this.status = status;
+    this.name = "DatabaseError";
+    this.body = body;
   }
 }
 
-type Types = Record<string, string>
+type Types = Record<string, string>;
 
 export interface ExecutedQuery {
-  headers: string[]
-  types: Types
-  rows: Row[]
-  size: number
-  statement: string
-  insertId: string | null
-  rowsAffected: number | null
-  error: VitessError | null
-  time: number
+  headers: string[];
+  types: Types;
+  rows: Row[];
+  size: number;
+  statement: string;
+  insertId: string | null;
+  rowsAffected: number | null;
+  error: VitessError | null;
+  time: number;
 }
 
-export type Cast = typeof cast
+export type Cast = typeof cast;
 
 export interface Config {
-  url?: string
-  username?: string
-  password?: string
-  host?: string
-  fetch?: (input: string, init?: ReqInit) => Promise<Pick<Response, 'ok' | 'json' | 'status' | 'statusText' | 'text'>>
-  format?: (query: string, args: any) => string
-  cast?: Cast
+  url?: string;
+  username?: string;
+  password?: string;
+  host?: string;
+  fetch?: (input: string, init?: ReqInit) => Promise<Pick<Response, "ok" | "json" | "status" | "statusText" | "text">>;
+  format?: (query: string, args: any) => string;
+  cast?: Cast;
 }
 
 interface QueryResultRow {
-  lengths: string[]
-  values: string
+  lengths: string[];
+  values: string;
 }
 
 export interface Field {
-  name: string
-  type: string
-  table?: string
+  name: string;
+  type: string;
+  table?: string;
 
   // Only populated for included fields
-  orgTable?: string | null
-  database?: string | null
-  orgName?: string | null
+  orgTable?: string | null;
+  database?: string | null;
+  orgName?: string | null;
 
-  columnLength?: number | null
-  charset?: number | null
-  flags?: number | null
-  columnType?: string | null
+  columnLength?: number | null;
+  charset?: number | null;
+  flags?: number | null;
+  columnType?: string | null;
 }
 
-type QuerySession = unknown
+type QuerySession = unknown;
 
 interface QueryExecuteResponse {
-  session: QuerySession
-  result: QueryResult | null
-  error?: VitessError
+  session: QuerySession;
+  result: QueryResult | null;
+  error?: VitessError;
 }
 
 interface QueryResult {
-  rowsAffected?: string | null
-  insertId?: string | null
-  fields?: Field[] | null
-  rows?: QueryResultRow[]
+  rowsAffected?: string | null;
+  insertId?: string | null;
+  fields?: Field[] | null;
+  rows?: QueryResultRow[];
 }
 
 export class Client {
-  private config: Config
+  private config: Config;
 
   constructor(config: Config) {
-    this.config = config
+    this.config = config;
   }
 
   async execute(query: string, args?: object | any[]): Promise<ExecutedQuery> {
-    return this.connection().execute(query, args)
+    return this.connection().execute(query, args);
   }
 
   connection(): Connection {
-    return new Connection(this.config)
+    return new Connection(this.config);
   }
 }
 
 export class Connection {
-  private config: Config
-  private session: QuerySession | null
+  private config: Config;
+  private session: QuerySession | null;
 
   constructor(config: Config) {
-    this.session = null
-    this.config = { ...config }
+    this.session = null;
+    this.config = { ...config };
 
-    if (typeof fetch !== 'undefined') {
-      this.config.fetch ||= fetch
+    if (typeof fetch !== "undefined") {
+      this.config.fetch ||= fetch;
     }
 
     if (config.url) {
-      const url = new URL(config.url)
-      this.config.username = url.username
-      this.config.password = url.password
-      this.config.host = url.hostname
+      const url = new URL(config.url);
+      this.config.username = url.username;
+      this.config.password = url.password;
+      this.config.host = url.hostname;
     }
   }
 
   async refresh(): Promise<void> {
-    await this.createSession()
+    await this.createSession();
   }
 
   async execute(query: string, args?: any): Promise<ExecutedQuery> {
-    const url = new URL('/psdb.v1alpha1.Database/Execute', `https://${this.config.host}`)
+    const url = new URL("/psdb.v1alpha1.Database/Execute", `https://${this.config.host}`);
 
-    const formatter = this.config.format || format
-    const sql = args ? formatter(query, args) : query
+    const formatter = this.config.format || format;
+    const sql = args ? formatter(query, args) : query;
 
-    const start = Date.now()
-    const saved = await postJSON<QueryExecuteResponse>(this.config, url, { query: sql, session: this.session })
-    const time = Date.now() - start
+    const start = Date.now();
+    const saved = await postJSON<QueryExecuteResponse>(this.config, url, { query: sql, session: this.session });
+    const time = Date.now() - start;
 
-    const { result, session, error } = saved
-    const rowsAffected = result?.rowsAffected ? parseInt(result.rowsAffected, 10) : null
-    const insertId = result?.insertId ?? null
+    const { result, session, error } = saved;
+    const rowsAffected = result?.rowsAffected ? parseInt(result.rowsAffected, 10) : null;
+    const insertId = result?.insertId ?? null;
 
-    this.session = session
+    this.session = session;
 
-    const rows = result ? parse(result, this.config.cast || cast) : []
-    const headers = result ? result.fields?.map((f) => f.name) ?? [] : []
+    const rows = result ? parse(result, this.config.cast || cast) : [];
+    const headers = result ? result.fields?.map((f) => f.name) ?? [] : [];
 
-    const typeByName = (acc, { name, type }) => ({ ...acc, [name]: type })
-    const types = result ? result.fields?.reduce<Types>(typeByName, {}) ?? {} : {}
+    const types = result
+      ? result.fields?.reduce<Types>((acc, { name, type }) => ({ ...acc, [name]: type }), {}) ?? {}
+      : {};
 
     return {
       headers,
@@ -159,113 +160,117 @@ export class Connection {
       error: error ?? null,
       size: rows.length,
       statement: sql,
-      time
-    }
+      time,
+    };
   }
 
   private async createSession(): Promise<QuerySession> {
-    const url = new URL('/psdb.v1alpha1.Database/CreateSession', `https://${this.config.host}`)
-    const { session } = await postJSON<QueryExecuteResponse>(this.config, url)
-    this.session = session
-    return session
+    const url = new URL("/psdb.v1alpha1.Database/CreateSession", `https://${this.config.host}`);
+    const { session } = await postJSON<QueryExecuteResponse>(this.config, url);
+    this.session = session;
+    return session;
   }
 }
 
-async function postJSON<T>(config: Config, url: string | URL, body = {}): Promise<T> {
-  const auth = btoa(`${config.username}:${config.password}`)
-  const { fetch } = config
-  const response = await fetch(url.toString(), {
-    method: 'POST',
-    body: JSON.stringify(body),
+async function postJSON<T>(
+  config: Config,
+  url: string | URL,
+  body: Record<string, unknown> = {},
+): Promise<T> {
+  const auth = btoa(`${config.username}:${config.password}`);
+  const { fetch } = config;
+  const response = await fetch!(url.toString(), {
+    method: "POST",
+    body: JSON.stringify(body).toString(),
     headers: {
-      'Content-Type': 'application/json',
-      'User-Agent': `database-js/${Version}`,
-      Authorization: `Basic ${auth}`
-    }
-  })
+      "Content-Type": "application/json",
+      "User-Agent": `database-js/${Version}`,
+      Authorization: `Basic ${auth}`,
+    },
+  });
 
   if (response.ok) {
-    return await response.json()
+    return await response.json();
   } else {
-    let error = null
+    let error = null;
     try {
-      const e = (await response.json()).error
-      error = new DatabaseError(e.message, response.status, e)
+      const e = (await response.json() as any).error;
+      error = new DatabaseError(e.message, response.status, e);
     } catch {
       error = new DatabaseError(response.statusText, response.status, {
-        code: 'internal',
-        message: response.statusText
-      })
+        code: "internal",
+        message: response.statusText,
+      });
     }
-    throw error
+    throw error;
   }
 }
 
 export function connect(config: Config): Connection {
-  return new Connection(config)
+  return new Connection(config);
 }
 
 function parseRow(fields: Field[], rawRow: QueryResultRow, cast: Cast): Row {
-  const row = decodeRow(rawRow)
+  const row = decodeRow(rawRow);
   return fields.reduce((acc, field, ix) => {
-    acc[field.name] = cast(field, row[ix])
-    return acc
-  }, {} as Row)
+    acc[field.name] = cast(field, row[ix]);
+    return acc;
+  }, {} as Row);
 }
 
 function parse(result: QueryResult, cast: Cast): Row[] {
-  const fields = result.fields
-  const rows = result.rows ?? []
-  return rows.map((row) => parseRow(fields, row, cast))
+  const fields = result.fields ?? [];
+  const rows = result.rows ?? [];
+  return rows.map((row) => parseRow(fields, row, cast));
 }
 
 function decodeRow(row: QueryResultRow): Array<string | null> {
-  const values = atob(row.values)
-  let offset = 0
+  const values = atob(row.values);
+  let offset = 0;
   return row.lengths.map((size) => {
-    const width = parseInt(size, 10)
+    const width = parseInt(size, 10);
     // Negative length indicates a null value.
-    if (width < 0) return null
-    const splice = values.substring(offset, offset + width)
-    offset += width
-    return splice
-  })
+    if (width < 0) return null;
+    const splice = values.substring(offset, offset + width);
+    offset += width;
+    return splice;
+  });
 }
 
 export function cast(field: Field, value: string | null): number | string | null {
-  if (value === '' || value == null) {
-    return value
+  if (value === "" || value == null) {
+    return value;
   }
 
   switch (field.type) {
-    case 'INT8':
-    case 'INT16':
-    case 'INT24':
-    case 'INT32':
-    case 'UINT8':
-    case 'UINT16':
-    case 'UINT24':
-    case 'UINT32':
-    case 'YEAR':
-      return parseInt(value, 10)
-    case 'FLOAT32':
-    case 'FLOAT64':
-      return parseFloat(value)
-    case 'DECIMAL':
-    case 'INT64':
-    case 'UINT64':
-    case 'DATE':
-    case 'TIME':
-    case 'DATETIME':
-    case 'TIMESTAMP':
-    case 'BLOB':
-    case 'BIT':
-    case 'VARBINARY':
-    case 'BINARY':
-      return value
-    case 'JSON':
-      return JSON.parse(decode(value))
+    case "INT8":
+    case "INT16":
+    case "INT24":
+    case "INT32":
+    case "UINT8":
+    case "UINT16":
+    case "UINT24":
+    case "UINT32":
+    case "YEAR":
+      return parseInt(value, 10);
+    case "FLOAT32":
+    case "FLOAT64":
+      return parseFloat(value);
+    case "DECIMAL":
+    case "INT64":
+    case "UINT64":
+    case "DATE":
+    case "TIME":
+    case "DATETIME":
+    case "TIMESTAMP":
+    case "BLOB":
+    case "BIT":
+    case "VARBINARY":
+    case "BINARY":
+      return value;
+    case "JSON":
+      return JSON.parse(decode(value));
     default:
-      return decode(value)
+      return decode(value);
   }
 }
--- src/text.ts
+++ src/text.ts
@@ -1,14 +1,14 @@
-const decoder = new TextDecoder('utf-8')
+const decoder = new TextDecoder("utf-8");
 
 export function decode(text: string | null): string {
-  return text ? decoder.decode(Uint8Array.from(bytes(text))) : ''
+  return text ? decoder.decode(Uint8Array.from(bytes(text))) : "";
 }
 
 export function hex(text: string): string {
-  const digits = bytes(text).map((b) => b.toString(16).padStart(2, '0'))
-  return `0x${digits.join('')}`
+  const digits = bytes(text).map((b) => b.toString(16).padStart(2, "0"));
+  return `0x${digits.join("")}`;
 }
 
 function bytes(text: string): number[] {
-  return text.split('').map((c) => c.charCodeAt(0))
+  return text.split("").map((c) => c.charCodeAt(0));
 }
